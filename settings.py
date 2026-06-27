25672031bdbf7495ffd38fbc4bfa9c4389415575
kl2508019968-college <kl2508019968@student.uptm.edu.my>
2026-06-28 04:58:03 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,17 @@
+# settings.py
+WIDTH = 950
+HEIGHT = 650
+FPS = 60
+TITLE = "Farm Rescue Puzzle Adventure"
+
+# Palet Warna Antara Muka Moden (Lebih Kemas & Menarik)
+COLOR_BG = (15, 18, 26)
+WHITE = (245, 245, 250)
+BLACK = (20, 22, 30)
+SKY_BLUE = (140, 210, 250)
+RIVER_BLUE = (35, 120, 230)
+LAND_GREEN = (45, 155, 85)
+WOOD_BROWN = (145, 75, 35)
+GOLD = (255, 210, 0)
+CRIMSON = (225, 45, 65)
+UI_NAVY = (28, 36, 52)
